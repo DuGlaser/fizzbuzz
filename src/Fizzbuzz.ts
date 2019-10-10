4eaf85308b4dbbfd248df3e25f454286b3e43451
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-10-11 00:28:47 +0900

    ADD src/Fizzbuzz.js addForm method

--- src/Fizzbuzz.ts
+++ src/Fizzbuzz.ts
@@ -1,2 +1,36 @@
-class Fizzbuzz {}
+class Fizzbuzz {
+    private formInfo = [];
+    private formInfoCounter: number = 0;
+    max: number;
+    min: number;
+    constructor(min: string, max: string) {
+        this.min = Number(min);
+        this.max = Number(max);
+    }
+
+    addForm(name: string, value: string) {
+        const inputName = document.createElement('input');
+        inputName.type = 'text';
+        inputName.value = name;
+
+        const inputValue: HTMLInputElement = document.createElement('input');
+        inputValue.type = 'number';
+        inputValue.value = value;
+
+        document.getElementById('formArea').appendChild(inputName);
+        document.getElementById('formArea').appendChild(inputValue);
+
+        this.formInfo.push({ text: name, num: Number(value) });
+        inputName.onchange = () => {
+            this.updateFormInfo(inputName);
+        };
+        inputValue.onchange = () => {
+            this.updateFormInfo(inputValue);
+        };
+
+        this.formInfoCounter++;
+    }
+
+    displyfizzbuzz() {}
+}
 export default Fizzbuzz;
