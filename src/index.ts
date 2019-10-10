afa2c8703d50541b0549101d06024966bc354115
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-10-11 03:04:30 +0900

    ADD create fizzbuzz component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import Fizzbuzz from './Fizzbuzz';
 
-const fizzbuzz = new Fizzbuzz();
+const RangeMin = <HTMLInputElement>document.getElementById('min-range');
+const RangeMax = <HTMLInputElement>document.getElementById('max-range');
+
+const fizzbuzz = new Fizzbuzz(RangeMin.value, RangeMax.value);
+fizzbuzz.addForm('fizz', '3');
+fizzbuzz.addForm('buzz', '5');
+fizzbuzz.displyfizzbuzz();
+
+// min-rangeが変更されたらクラスのminを更新する
+RangeMin.addEventListener('change', () => {
+    fizzbuzz.setMin(RangeMin.value);
+});
+
+// max-rangeが変更されたらクラスのmaxを更新する
+RangeMax.addEventListener('change', () => {
+    fizzbuzz.setMax(RangeMax.value);
+});
