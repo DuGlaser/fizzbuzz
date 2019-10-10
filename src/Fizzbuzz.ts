e451064a5382278201988d718f0772f02d40db52
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-10-11 03:03:53 +0900

    ADD src/Fizzbuzz.ts displayfizzbuzz method

--- src/Fizzbuzz.ts
+++ src/Fizzbuzz.ts
@@ -7,9 +7,11 @@ class Fizzbuzz {
         this.min = Number(min);
         this.max = Number(max);
     }
-
+    // [forInfoに値を設定するためのInput要素を生成するメソッド]
     addForm(name: string, value: string) {
-        const inputName = document.createElement('input');
+        const wrapper: HTMLElement = document.createElement('div');
+        wrapper.className = 'wrapper';
+        const inputName: HTMLInputElement = document.createElement('input');
         inputName.type = 'text';
         inputName.value = name;
 
@@ -17,23 +19,72 @@ class Fizzbuzz {
         inputValue.type = 'number';
         inputValue.value = value;
 
-        document.getElementById('formArea').appendChild(inputName);
-        document.getElementById('formArea').appendChild(inputValue);
+        wrapper.appendChild(inputName);
+        wrapper.appendChild(inputValue);
+        document.getElementById('formArea').appendChild(wrapper);
 
-        this.formInfo.push({ text: name, num: Number(value) });
+        this.formInfo.push({ text: name, value: Number(value) });
 
-        this.updateFormInfo(inputName);
-        this.updateFormInfo(inputValue);
+        const count: number = this.formInfoCounter;
+
+        inputName.onchange = () => {
+            this.formInfo[count].text = inputName.value;
+            this.displyfizzbuzz();
+        };
+        inputValue.onchange = () => {
+            this.formInfo[count].value = Number(inputValue.value);
+            this.displyfizzbuzz();
+        };
 
         this.formInfoCounter++;
     }
 
-    updateFormInfo(target: HTMLInputElement) {
-        target.onchange = () => {
-            this.formInfo[this.formInfoCounter].text = target.value;
-            this.displyfizzbuzz();
-        };
+    setMin(Min) {
+        this.min = Number(Min);
+        this.displyfizzbuzz();
     }
-    displyfizzbuzz() {}
+
+    setMax(Max) {
+        this.max = Number(Max);
+        this.displyfizzbuzz();
+    }
+    // [formInfoのvalueの倍数が範囲内にあるかをcontainsを使って調べ、結果をfizzbuzzListに格納し
+    // html上に表示するメソッド]
+    displyfizzbuzz() {
+        const container: HTMLElement = document.querySelector('#container ul');
+        container.innerHTML = null;
+        const contains: Array<number> = [];
+        const fizzbuzzList: Array<string> = [];
+        const min: number = this.min;
+        const max: number = this.max;
+
+        for (let i = min; i <= max; i++) {
+            contains.push(i);
+            fizzbuzzList.push(`<p>${i}</p>`);
+        }
+
+        this.formInfo.forEach(el => {
+            for (let j = el.value; j <= max; j += el.value) {
+                // findIndexはcontainsに値があるときその添字を返し、ないときは－１を返す
+                const index: number = contains.findIndex(
+                    (val: number) => val === j
+                );
+                if (index !== -1) {
+                    // textが入れられたことがなければ中身を置き換える
+                    if (fizzbuzzList[index] === `<p>${j}</p>`) {
+                        fizzbuzzList[index] = `<p>${el.text}</p>`;
+                    } else {
+                        fizzbuzzList[index] =
+                            fizzbuzzList[index] + `<p>${el.text}</p>`;
+                    }
+                }
+            }
+        });
+        for (let i = 0; i <= max - min; i++) {
+            const list: HTMLElement = document.createElement('li');
+            list.innerHTML = fizzbuzzList[i];
+            container.appendChild(list);
+        }
+    }
 }
 export default Fizzbuzz;
